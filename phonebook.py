c52954750cd70c7f1826ee39c064f63e381b422e
Zay <hosea8zay@gmail.com>
2022-02-24 14:12:55 -0500

    corrected the phonebook value

--- phonebook.py
+++ phonebook.py
@@ -15,9 +15,9 @@ print(menu)
 selected_option = input("what do you want to do (1-5)?")
 
 if selected_option == "2":
-    name = input("what is the contact's name?")
-    phone_number = input("what is their phone number?")
-    phone_number[name] = phone_number
+    name = input("what is the contact's name? ")
+    phone_number = input("what is their phone number? ")
+    phonebook[name] = phone_number
     print("Contact added Successfully")
     print(menu)
 
